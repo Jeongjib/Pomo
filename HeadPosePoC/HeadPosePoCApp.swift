8f5ca24cefc4b84a9c1dfffe8f996f194cf12327
Jeongjib <house07061@gmail.com>
2026-05-24 12:33:27 +0900

    Initial Commit

---
+++ HeadPosePoC/HeadPosePoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadPosePoCApp.swift
+//  HeadPosePoC
+//
+//  Created by 박정집 on 5/24/26.
+//
+
+import SwiftUI
+
+@main
+struct HeadPosePoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
